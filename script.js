1c390035afbcc197d3ec7bd55dda2a56027f4e92
Rick Levy <ricklevy21@gmail.com>
2020-04-06 15:45:02 -0600

    added starter files and began commenting out script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Assignment Code
+//Create variable that returns the button element in the html file with id="generate"
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
